adcbc6a4cbe71810e6744c044183904a576820fc
Héctor Tello <hectortllo@gmail.com>
2021-03-13 11:49:19 -0600

    1. Se creó un nuevo controlador para los posts 2. Se modificaron las rutas para acceder a los posts 3. Se crearon las vistas para ver todos los posts y un post en específico

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [PageController::class, 'posts']);
+Route::get('blog/{post}', [PageController::class, 'post'])->name('post');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
