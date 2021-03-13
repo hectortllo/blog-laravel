0961e38fe228fa0e0b4c6f34d67140d8697908de
Héctor Tello <hectortllo@gmail.com>
2021-03-13 12:03:11 -0600

    1. Se agregó el método para hacer el extracto del body del post 2. Se agregó la parte de la paginación

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -29,4 +29,8 @@ class Post extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function getGetExcerptAttribute() {
+        return substr($this->body, 0, 140);
+    }
 }
